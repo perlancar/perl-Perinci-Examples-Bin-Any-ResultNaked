9fca8b35af487a7f224949bccee6dcdea3d0fdf9
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-26 15:19:54 +0700

    Initial commit

---
+++ lib/Perinci/Examples/Bin/Any/ArgsAs.pm
@@ -0,0 +1,20 @@
+package Perinci::Examples::Bin::Any::ArgsAs;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Example CLI scripts to test various `args_as` values
+
+=head1 DESCRIPTION
+
+This distribution contains the following scripts:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Perinci::Examples::ArgsAs>
